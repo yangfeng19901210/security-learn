f05ab3fc54d19cb0eb75f9a6ffa3fca1bca662ce
15525438831 <15525438831@163.com>
2025-07-29 14:10:59 +0800

    先有类以后如何添加注释

--- src/main/java/com/yy/controller/IndexController.java
+++ src/main/java/com/yy/controller/IndexController.java
@@ -2,11 +2,12 @@ package com.yy.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 /**
- * Controller for "/".
- *
- * @author Joe Grandja
+ * @ClassName IndexController
+ * @Description 首页控制器
+ * @Author yangfeng
+ * @Date 2025/7/29 14:09
+ * @Version 1.0
  */
 @Controller
 public class IndexController {
